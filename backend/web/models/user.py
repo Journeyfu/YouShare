5036a065a3472cb63cc3e42da761f9233eb044ae
Ziang Fu <ziang_fu@163.com>
2026-01-30 13:50:03 +1300

    update profile and implement dataset

---
+++ backend/web/models/user.py
@@ -0,0 +1,29 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now, localtime
+
+
+def photo_upload_to(instance, filename):
+    ext = filename.split('.')[-1]
+    # uuid.uuid4() is a 36-bit random string including both digit numbers and letters; hex: base 16
+    filename = f'{uuid.uuid4().hex[:10]}.{ext}'
+    return f'user/photos/{instance.user_id}_{filename}'
+
+
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(default='user/photos/default.jpg', upload_to=photo_upload_to)
+    profile = models.TextField(default="Thanks for your subscribing", max_length=500) # max_length is not valid
+    create_time = models.DateTimeField(default=now)
+    update_time = models.DateTimeField(default=now)
+
+    def __str__(self): # the format of userprofile name showed in the localhost:8000/admin
+        return f'{self.user.username} - {localtime(self.create_time).strftime('%Y-%m-%d %H:%M:%S')}'
+
+
+
+
