3af8b06812ad7f2340c1c3c7048eb2a77de4fe79
Ziang Fu <ziang_fu@163.com>
2026-02-08 17:19:43 +1300

    implement updating character frontend

--- backend/web/views/create/character/get_single.py
+++ backend/web/views/create/character/get_single.py
@@ -12,7 +12,7 @@ class GetSingleCharacterView(APIView):
             character_id = request.query_params.get('character_id')
             character = Character.objects.get(id=character_id, author__user=request.user)
             return Response({
-                'result': success,
+                'result': "success",
                 'character': {
                     'id': character.id,
                     'name': character.name,
